a743da73566c7e2d693610b55a7f4e479980846e
Satoe Sakonjo <satoesakonjo@satoe-mbp.local>
2022-10-26 12:37:08 -0700

    Modularize the connect function and imported it

--- play.js
+++ play.js
@@ -1,21 +1,7 @@
 const net = require("net");
+const connect = require("./client");
 
-// establishes a connection with the game server
-const connect = function () {
-    const conn = net.createConnection({
-        host: "192.168.1.246",
-        port: 50541,
-    });
 
-    // interpret incoming data as text
-    conn.setEncoding("utf8");
-
-    conn.on("data", (data) => {
-        console.log(data);
-    });
-
-    return conn;
-};
 
 console.log("Connecting ...");
 connect();
