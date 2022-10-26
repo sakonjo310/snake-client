bbda94180748cea7faa1d24ebaf20e906fd55801
Satoe Sakonjo <satoesakonjo@satoe-mbp.local>
2022-10-26 16:48:41 -0700

    Create a file to store some constant variables

---
+++ constants.js
@@ -0,0 +1,17 @@
+const IP = "165.227.47.243";
+const PORT = 50541;
+const NAME = "SSK";
+const WASD = {
+    w: "Move: up",
+    a: "Move: left",
+    s: "Move: down",
+    d: "Move: right",
+    q: "Say: henlo"
+}
+
+module.exports = {
+    IP,
+    PORT,
+    NAME,
+    WASD
+};
