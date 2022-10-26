2dabbcc9321cf15300d7bbf460614cacd9a82e4a
Satoe Sakonjo <satoesakonjo@satoe-mbp.local>
2022-10-26 14:15:39 -0700

    Create a function to let the user quit the game

--- play.js
+++ play.js
@@ -1,7 +1,24 @@
 const net = require("net");
+
 const connect = require("./client");
 
+const setupInput = function () {
+    const stdin = process.stdin;
+
+    const handleUserInput = function (key) {
+        if (key === '\u0003') {
+            process.exit();
+        }
+    }
+
+    stdin.on("data", handleUserInput);
+    stdin.setRawMode(true);
+    stdin.setEncoding("utf8");
+    stdin.resume();
+    return stdin;
+};
 
 
 console.log("Connecting ...");
 connect();
+setupInput();
