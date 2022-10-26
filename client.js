0b2c9db8b95a12ceb76cbb9529d9b405628db298
Satoe Sakonjo <satoesakonjo@satoe-mbp.local>
2022-10-26 13:17:33 -0700

    Send initials to the server

--- client.js
+++ client.js
@@ -9,9 +9,17 @@ const connect = function () {
     // interpret incoming data as text
     conn.setEncoding("utf8");
 
+    conn.on("connect", () => {
+        console.log("Successfully connected to game server");
+        conn.write("Name: SSK");
+    })
+
     conn.on("data", (data) => {
         console.log(data);
-    });
+
+    })
+
+
 
     return conn;
 };
