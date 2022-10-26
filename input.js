897bc02c98e9e982f8eb418cb6deea56bb5de1a1
Satoe Sakonjo <satoesakonjo@satoe-mbp.local>
2022-10-26 16:50:29 -0700

    Modularize the variables

--- input.js
+++ input.js
@@ -1,19 +1,27 @@
 const { setgroups } = require("process");
+const { WASD } = require("./constants");
+let connection;
 
-const setupInput = function () {
+
+const handleUserInput = function (key) {
+    if (key === '\u0003') {
+        process.exit();
+    }
+    if (WASD[key]) {
+        connection.write(WASD[key]);
+    }
+};
+
+const setupInput = function (conn) {
+    connection = conn;
     const stdin = process.stdin;
 
-    const handleUserInput = function (key) {
-        if (key === '\u0003') {
-            process.exit();
-        }
-    }
-
+    stdin.setEncoding("utf8");
     stdin.on("data", handleUserInput);
     stdin.setRawMode(true);
-    stdin.setEncoding("utf8");
+
     stdin.resume();
     return stdin;
 };
 
-module.exports = setupInput;
+module.exports = { setupInput };
